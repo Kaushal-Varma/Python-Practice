acd24860836ea66a1b60a4bff777fd18f5e2d290
Manthena Sai Kaushal Varma <manthenasaikaushalvarma.25.csm@anits.edu.in>
2026-09-26 20:36:58 +0530

    Basic Programs 3

--- Basics/LCM.py
+++ Basics/LCM.py
@@ -1,9 +1,24 @@
 class Solution:
     def LCM(self,n1: int, n2: int) -> int:
+        if n1 > n2:
+            n = n1
+        else:
+            n = n2
 
+        while True:
+            if n % n1 == 0 and n % n2 == 0:
+                return n
+            n += 1
 
 def main():
-    print("hello world")
+    s = Solution()
+
+    a = 6
+    b = 8
+
+    result = s.LCM(a, b)
+
+    print("LCM =", result)
 
 if __name__ == "__main__":
     main()
